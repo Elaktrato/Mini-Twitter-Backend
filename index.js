d4357a01bc146f29401bd6226a3ee73675258c2a
Erik <elaktrato@gmail.com>
2021-11-04 21:01:34 +0100

    Setup messages

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const { getAllMessages, getSingleUser } = require('./controllers');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.get('/messages', (req, res) => {
-    res.send('not implemented yet')
+app.get('/messages', async(req, res) => {
+    try {
+        return res.send(await getAllMessages())
+    } catch (err) {
+        return res.status(400).send(err.message)
+    }
 })
 
 app.get('/messages/:id', (req, res) => {
@@ -20,8 +25,12 @@ app.get('/users', (req, res) => {
     res.send('not implemented yet')
 })
 
-app.get('/users/:id', (req, res) => {
-    res.send('not implemented yet')
+app.get('/users/:id', async(req, res) => {
+    try {
+        return res.send(await getSingleUser(req.params.id))
+    } catch (err) {
+        return res.status(400).send(err.message)
+    }
 })
 
 app.listen(PORT, () => {
