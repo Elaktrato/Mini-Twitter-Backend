eff5701bea2977d33bba82375dcd93bf6b32a7da
Erik <elaktrato@gmail.com>
2021-11-04 19:21:42 +0100

    barebones express app setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 8080;
+
+app.get('/messages', (req, res) => {
+    res.send('not implemented yet')
+})
+
+app.get('/messages/:id', (req, res) => {
+    res.send('not implemented yet')
+})
+
+app.get('/users', (req, res) => {
+    res.send('not implemented yet')
+})
+
+app.get('/users/:id', (req, res) => {
+    res.send('not implemented yet')
+})
+
+app.listen(PORT, () => {
+    console.log("API running on " + PORT)
+})
